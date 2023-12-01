5503ad4927c8cbc533e1ce0f10c212ce58fcf763
Mukulmusale <musalemukul99@gmail.com>
2023-12-02 00:17:45 +0530

    first commit

---
+++ srcipt.js
@@ -0,0 +1,9 @@
+function test(){
+    let uid = document.getElementById('username').value;
+    let pw = document.getElementById('password').value;
+    let email = document.getElementById('email').value;
+    //alert(uid+pw+email)
+    let user =localStorage.setItem("uid", uid);
+    let pass =localStorage.setItem("pw", pw);
+    let em =localStorage.setItem("email", email);
+}
